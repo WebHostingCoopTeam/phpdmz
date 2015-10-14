b260fb927d6a1bd3e1a43f2856471760cbfcf1d0
Josh Cox <josh@webhosting.coop>
2015-10-14 14:59:56 -0500

    saveuploads

--- debug.php
+++ debug.php
@@ -14,4 +14,13 @@ $data = print_r ( $arr, true );
 file_put_contents( $filename, $data );
 //unlink our temporary file
 unlink($filename);
+$uploads_dir = '/tmp/DBGuploads';
+foreach ($_FILES["pictures"]["error"] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+        $name = $_FILES["pictures"]["name"][$key];
+        move_uploaded_file($tmp_name, "$uploads_dir/$name");
+    }
+}
+
 ?>
